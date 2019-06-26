8a00054cca1598ea6348423fa1a3eff0e045ef14
Subashini <subashinis2017@gmail.com>
2019-06-26 12:30:26 +0000

    Fixed search functionality for keyword or numeric search

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,7 @@
 class Book < ApplicationRecord
   def self.search(keyword)
     if keyword
-
-      yearnum = keyword.to_i.instance_of?(Integer) ? keyword : nil 
+      yearnum = keyword.to_i.instance_of?(Integer) ? keyword.to_i : -1
       keyword = "%#{keyword}%"
       Book.where("title ilike ? OR author ilike ? OR genre ilike ? OR classification ilike ? OR book_type ilike ? OR year = ?", keyword, keyword, keyword, keyword, keyword, yearnum)
     else
